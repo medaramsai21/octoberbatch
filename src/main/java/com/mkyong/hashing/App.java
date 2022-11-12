6e95ee10acb1cef5fe68f923e13ea2f37bc8d931
NageshDevopscontent <87036876+NageshDevopscontent@users.noreply.github.com>
2022-11-12 15:00:41 +0000

    Update App.java

--- src/main/java/com/mkyong/hashing/App.java
+++ src/main/java/com/mkyong/hashing/App.java
@@ -6,7 +6,7 @@ public class App {
 
     public static void main(String[] args) {
 
-      System.out.println("A java based maven poject")
+      System.out.println("A java based maven poject");
     }
 
 }
